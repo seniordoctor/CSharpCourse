edec5a9f438d2c1de0ac03f9ed558ec2831a546a
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-03-06 20:39:07 +0300

    Master #3

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -8,6 +8,10 @@ namespace Classes
 {
 	class Customer
 	{
+		//field
+		//public string FirstName;
+
+		//property
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
