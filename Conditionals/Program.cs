490325e647a180998aa47697c7296d2532b953ea
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-03-06 11:52:06 +0300

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+
+            // kısa if blogu:
+            // Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("number is 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("number is 20");
+            //        break;
+            //    default:
+            //        Console.WriteLine("number is not 10 or 20");
+            //        break;
+            //}
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
