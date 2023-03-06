58b9afc08e16dc5f6e4f843b58e16304bfc8faa4
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-03-06 16:55:13 +0300

    Master #2

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -18,6 +18,19 @@ namespace Classes
             productManager.Add();
             productManager.Update();
 
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Recep";
+            customer.LastName = "MERT";
+            customer.City = "Istanbul";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, FirstName = "Recep", LastName = "MERT", City = "Ankara"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
             Console.ReadLine();
         }
     }
