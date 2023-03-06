ed3c5413f4fd503b701a6de165e654f2ba8d58ae
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-03-06 16:34:29 +0300

    Master #2

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -22,6 +22,8 @@ namespace Methods
             Console.WriteLine(Multiply(2, 4));
             Console.WriteLine(Multiply(2, 4, 5));
 
+            Console.WriteLine(Add4(2, 3, 4, 5, 6, 7, 8));
+
             Console.ReadLine();
         }
 
@@ -53,6 +55,12 @@ namespace Methods
         static int Multiply(int number1, int number2, int number3)
         {
             return number1 * number2 * number3;
+        }
+
+        // Method Overloading yapamayacak kadar fazla int veya farkli veri tipinde deger girilecegi zaman params keyword ile calisabiliriz.
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();   // Sum() ile params icindekileri toplayacak.
         }
     }
 }
---
+++ Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro();
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
+            string city = "Ankara";
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "Istanbul";
+
+            Console.WriteLine(string.Format("{0}{1}", city, city2));
+        }
+    }
+}
