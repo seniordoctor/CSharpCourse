54a512818a3b7cc3040f92d0cdc5141aa247a275
seniordoctor <recepmert034@gmail.com>
2023-03-11 23:44:48 +0300

    Master 6

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[2] { "Ankara", "İstanbul" };
+            cities = new string[3];
+            Console.WriteLine(cities[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// bir sinif genel anlamda newlendiginde calisacak kod blogudur
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //CustomerManager() parantez acip kapama - o sinifin parametresiz sekilde calismastirilmasi anlamina gelir
+            customerManager.List();
+
+            Product product = new Product { Id = 1, Name = "Laptop" };
+            Product product2 = new Product(2, "Computer");
+
+            EmployeeManager employeeManager = new EmployeeManager(new FileLogger());
+            employeeManager.Add();
+
+            PersonManager personManager = new PersonManager("Product");
+            personManager.Add();
+
+            Teacher.Number = 10;
+
+            Utilities.Validate();
+
+            Manager.DoSomething(); // static direkt sinif uzerinden cagirilir
+            Manager manager = new Manager(); // static olmayani instance uzerinden cagiririz
+            manager.DoSometing2();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        private int _count = 15; // private bir field alt cizgi ile baslatilir
+        public CustomerManager(int count) // metod halinde alt cizgi kullanilmaz
+        {
+            _count = count;
+        }
+        public CustomerManager()
+        {
+
+        }
+        public void List()
+        {
+            Console.WriteLine("Listed! {0} items", _count);
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added! {0} items", _count);
+        }
+    }
+
+    class Product
+    {
+        public Product()
+        {
+
+        }
+        int _id;
+        string _name;
+        public Product(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+    }
+
+    class EmployeeManager
+    {
+        private ILogger _logger;
+        public EmployeeManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            _logger.Log();
+            Console.WriteLine("Added!");
+        }
+    }
+
+    class BaseClass
+    {
+        private string _entity;
+        public BaseClass(string entity)
+        {
+            _entity = entity;
+        }
+        public void Message()
+        {
+            Console.WriteLine("{0} message", _entity);
+        }
+    }
+
+    class PersonManager : BaseClass
+    {
+        public PersonManager(string entity) : base(entity)
+        {
+
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added!");
+            Message(); 
+        }
+    }
+
+    static class Teacher
+    {
+        public static int Number { get; set; }
+    }
+
+    static class Utilities
+    {
+        static Utilities()
+        {
+
+        }
+        public static void Validate()
+        {
+            Console.WriteLine("Validation is done");
+        }
+    }
+
+    class Manager
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("Done");
+        }
+
+        public void DoSometing2()
+        {
+            Console.WriteLine("Done 2");
+        }
+    }
+}
