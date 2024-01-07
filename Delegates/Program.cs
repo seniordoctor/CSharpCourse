ffc740f14c4e9989d9678616b172843dc3f9582c
Recep Mert <doctorprivate@outlook.com>
2024-01-07 14:57:35 +0300

    delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates //delegate'e elci olarak orneklendirebilirsin
+{
+    public delegate void MyDelegate(); // void - bir sey dondurmeyecek
+    public delegate void MyDelegate2(string text);
+    public delegate int MyDelegate3(int number1, int number2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            Matematik matematik = new Matematik();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;    // arka arkaya iki operasyonu cagirmak icin kullanilabilir
+
+            myDelegate -= customerManager.SendMessage;
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.SendMessage2;
+
+            MyDelegate3 myDelegate3 = matematik.Topla;
+            myDelegate3 += matematik.Carp;
+            var sonuc = myDelegate3(2, 3);
+
+            Console.WriteLine(sonuc);
+
+            myDelegate2("Hello");
+            myDelegate();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+
+    public class Matematik
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
